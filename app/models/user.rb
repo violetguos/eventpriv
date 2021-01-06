043b840c31582a953d35626010bad66917c61c3e
Y. Violet Guo <violetguos@users.noreply.github.com>
2021-01-06 15:46:22 -0400

    Finally bypassed email in devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable, 
-       :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+          :rememberable,
+          :trackable,
+          :validatable,
+          :authentication_keys => [:username]
+
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
 end
