648f27ee1b6e05db0743e242bc4ec76c936f8206
Y. Violet Guo <violetguos@users.noreply.github.com>
2021-01-05 21:16:16 -0400

    WIP add devise sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.fetch(:user, {})
+      params.fetch(:user).permit(:name)
     end
 end
