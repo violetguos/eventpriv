e35d5c4fc16c8a006853fb0dfb894a05e1b697df
Y. Violet Guo <violetguos@users.noreply.github.com>
2021-01-15 14:28:10 -0400

    Add attend link in route and controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :logged_in?, except: [:index]
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :set_event, only: [:show, :edit, :update, :destroy, :attend]
 
   def index
     @events = Event.all
