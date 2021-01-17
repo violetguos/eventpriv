17f9c4b0b0662f5e226b07d228e168b93124c3bf
Y. Violet Guo <violetguos@users.noreply.github.com>
2021-01-16 22:14:01 -0400

    Separate past and future events, improve UI

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   has_many :event_attendings, class_name: "EventAttending", foreign_key: "attended_event_id"
   has_many :attendees, through: :event_attendings, source: :event_attendee
   belongs_to :user, class_name: "User", foreign_key: "host_id"
+
+  scope :past, -> { where("time < ?", Date.current) }
+  scope :upcoming, -> { where("time >= ?", Date.current) }
 end
