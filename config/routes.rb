2b55a52e4b9fdbb75cf8b3bb50d04f0648ab2d2b
Y. Violet Guo <violetguos@users.noreply.github.com>
2021-01-18 11:21:21 -0400

    Change root to login

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "events#index"
+  root to: "sessions#new"
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
